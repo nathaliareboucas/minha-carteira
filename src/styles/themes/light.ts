0927854140fc44259b96f3411d334521669389af
Nathália Rebouças <nathreboucas5@gmail.com>
2020-12-08 08:26:18 -0300

    criando os temas

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#dcdcdc',
+    secondary: '#fff',
+    tertiary: '#f5f5f5',
+
+    white: '#fff',
+    black: '#000',
+    gray: '#bfbfbf',
+
+    success: '#03bb85',
+    info: '#3b5998',
+    warning: '#ff6961'
+  },
+};
