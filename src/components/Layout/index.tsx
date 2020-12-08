f11ed545a8cea4ccc2ea3cd9c96c979f54664977
Nathália Rebouças <nathreboucas5@gmail.com>
2020-12-08 07:21:22 -0300

    criando componentes do layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {GridLayout} from './styles';
+import MainHeader from '../MainHeader';
+import Aside from '../Aside';
+import Content from '../Content';
+
+const Layout: React.FC = () => {
+  return(
+    <GridLayout>
+      <MainHeader />
+      <Aside />
+      <Content />
+    </GridLayout>
+  );
+};
+
+export default Layout;
