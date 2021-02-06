0f8d1a150ab3a65f2ea2fff0b994bff1bc30b54a
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-05 21:57:32 -0300

    criando o header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
-import {Container} from './styles';
+import {Container, Profile, Welcome, UserName} from './styles';
+import emojis from '../../utils/emojis';
 
 const MainHeader: React.FC = () => {
+
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return(
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Nathália Rebouças</UserName>
+      </Profile>
     </Container>
   );
 };
