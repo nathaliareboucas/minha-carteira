e63282a104053c72d47f7c8586a2bac0f2a0694d
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-09 00:58:29 -0300

    criando os filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,51 +1,37 @@
 import React from 'react';
 
-import {Container, Content} from './styles';
+import {Container, Content, Filters} from './styles';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
 
-  const options = [
-    {value: 'Maria', label: 'Maria'},
-    {value: 'João', label: 'João'},
+  const months = [
+    {value: 7, label: 'Julho'},
+    {value: 8, label: 'Agosto'},
+    {value: 9, label: 'Setembro'},
+  ];
+
+  const years = [
+    {value: 2021, label: 2021},
+    {value: 2020, label: 2020},
+    {value: 2019, label: 2019},
   ];
 
   return(
     <Container>
       <ContentHeader title="Saídas" lineColor="#E44C4E">
-        <SelectInput options={options}/>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">Recorrentes</button>
+        <button type="button" className="tag-filter tag-filter-eventual">Eventuais</button>
+      </Filters>
       
       <Content>
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
-        <HistoryFinanceCard tagColor="#E44C4E" 
-          title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
-
         <HistoryFinanceCard tagColor="#E44C4E" 
           title="Conta de luz" subtitle="08/02/2021" amount="R$ 70,00"/>
       </Content>
