09119445b38e6bdd98bc807d74effb0560ce2640
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-19 07:52:45 -0300

    formatando os valores da legenda do gráfico de histórico de saldos

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -1,66 +1,64 @@
 import React from 'react';
-import {ResponsiveContainer, LineChart, Line, XAxis, CartesianGrid, Tooltip} from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, CartesianGrid, Tooltip } from 'recharts';
 
-import {Container, ChartContainer, Header, LegendContainer, Legend} from './styles';
+import { Container, ChartContainer, Header, LegendContainer, Legend } from './styles';
+import formatCurrency from '../../utils/formatCurrency';
 
 interface IHistoryBoxProps {
-  data: {
-    month: string;
-    amountEntry: number;
-    amountOutput: number;
-  }[],
-  lineColorAmountEntry: string;
-  lineColorAmountOutput: string;
+	data: {
+		month: string;
+		amountEntry: number;
+		amountOutput: number;
+	}[];
+	lineColorAmountEntry: string;
+	lineColorAmountOutput: string;
 }
 
-const HistoryBox: React.FC<IHistoryBoxProps> = ({
-  data, lineColorAmountEntry, lineColorAmountOutput
-}) => (
-    <Container>
-      <Header>
-        <h2>Histórico de saldo</h2>
-        <LegendContainer>
-          <Legend color={lineColorAmountEntry}>
-            <div></div>
-            <span>Entradas</span>
-          </Legend>
+const HistoryBox: React.FC<IHistoryBoxProps> = ({ data, lineColorAmountEntry, lineColorAmountOutput }) => (
+	<Container>
+		<Header>
+			<h2>Histórico de saldo</h2>
+			<LegendContainer>
+				<Legend color={lineColorAmountEntry}>
+					<div />
+					<span>Entradas</span>
+				</Legend>
 
-          <Legend color={lineColorAmountOutput}>
-            <div></div>
-            <span>Saídas</span>
-          </Legend>
-        </LegendContainer>
-      </Header>
+				<Legend color={lineColorAmountOutput}>
+					<div />
+					<span>Saídas</span>
+				</Legend>
+			</LegendContainer>
+		</Header>
 
-      <ChartContainer>
-        <ResponsiveContainer>
-          <LineChart data={data} margin={{top:5, right:20, left:20, bottom:5}}>
-            <CartesianGrid strokeDasharray="3 3" stroke="#CECECE"/>
-            <XAxis dataKey="month" stroke="#CECECE"/>
-            <Tooltip/>
-            <Line 
-              type="monotone" 
-              dataKey="amountEntry" 
-              name="Entradas" 
-              stroke={lineColorAmountEntry}
-              strokeWidth={5} 
-              dot={{r:5}}
-              activeDot={{r:8}}
-            />
-            <Line 
-              type="monotone" 
-              dataKey="amountOutput" 
-              name="Saídas" 
-              stroke={lineColorAmountOutput}
-              strokeWidth={5} 
-              dot={{r:5}}
-              activeDot={{r:8}}
-            />          
-          </LineChart>
-        </ResponsiveContainer>
-        </ChartContainer>
-    </Container>
+		<ChartContainer>
+			<ResponsiveContainer>
+				<LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
+					<CartesianGrid strokeDasharray="3 3" stroke="#CECECE" />
+					<XAxis dataKey="month" stroke="#CECECE" />
+					<Tooltip formatter={(value: number) => formatCurrency(value)} />
+					<Line
+						type="monotone"
+						dataKey="amountEntry"
+						name="Entradas"
+						stroke={lineColorAmountEntry}
+						strokeWidth={5}
+						dot={{ r: 5 }}
+						activeDot={{ r: 8 }}
+					/>
+					<Line
+						type="monotone"
+						dataKey="amountOutput"
+						name="Saídas"
+						stroke={lineColorAmountOutput}
+						strokeWidth={5}
+						dot={{ r: 5 }}
+						activeDot={{ r: 8 }}
+					/>
+				</LineChart>
+			</ResponsiveContainer>
+		</ChartContainer>
+	</Container>
 );
-
 
 export default HistoryBox;
