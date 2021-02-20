f85e6b961f02ac0743124db5d30e36958ab6832e
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-20 09:04:37 -0300

    criando a tela de login

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import {Container, Logo, Form, FormTitle} from './styles';
+import logoImg from '../../assets/logo.svg';
+
 const Signin: React.FC = () => {
   return(
-    <h1>Signin</h1>
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha Carteira"/>
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+
+        <input type="text"></input>
+        <input type="text"></input>
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
-import AppRoutes from './app.routes';
+// import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
 
 const Routes: React.FC = () => (  
   <BrowserRouter>
-    <AppRoutes />  
+    <AuthRoutes/>
   </BrowserRouter>
 );
 
