7b962a3335542948584a6a5f94dd0a2674deb092
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-21 15:52:21 -0300

    ajustes finais no layout responsivo

--- src/components/ContentHeader/styles.ts
+++ src/components/ContentHeader/styles.ts
@@ -11,6 +11,10 @@ export const Container = styled.div`
   align-items: center;
 
   margin-bottom: 25px;
+
+  @media(max-width: 320px) {
+    flex-direction: column;    
+  }
 `;
 
 export const TitleContainer = styled.div<ITitleContainerProps>`
@@ -25,8 +29,27 @@ export const TitleContainer = styled.div<ITitleContainerProps>`
       border-bottom: 10px solid ${props => props.lineColor};
     }
   }
+
+  @media(max-width: 420px) {
+    > h1 {
+      font-size: 22px;
+
+      &::after {
+        content: '';
+        display: block;
+        width: 55px;
+        border-bottom: 7px solid ${props => props.lineColor};
+      }
+    }
+  }
 `;
 
 export const Controllers = styled.div`
   display: flex;
+
+  @media(max-width: 320px) {
+    width: 100%;
+    justify-content: space-around;
+    margin-top: 20px;
+  }
 `;
